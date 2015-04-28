efc37d47d2051011fd4ab4532bdaa608753d3e58
User <User@User-PC>
2015-04-27 23:03:14 -0400

    chap5 lab1 done

--- IntroducingMethods/src/cst1201/IntroducingMethods.java
+++ IntroducingMethods/src/cst1201/IntroducingMethods.java
@@ -11,34 +11,35 @@ public class IntroducingMethods {
 
     public static void main(String[] args) {
 
-        // TODO: Fill in the body with your code
+        System.out.print("Enter a string: ");
+        Scanner keyboard = new Scanner(System.in);
+        getInputString(keyboard);
     }
 
-    /**
-     * Given a Scanner, prompts the user for a String.  If the user enters an empty
-     * String, reports an error message and asks for a non-empty String.  Returns the
-     * String to the calling program.
-     * @param inScanner The Scanner to use to prompt the user.
-     * @return The resulting String from the user.
-     */
     private static String getInputString(Scanner inScanner) {
-        // TODO: Fill in the body
 
-        // NOTE: Do not declare a Scanner in the body of this method.
+        String input = inScanner.nextLine();
+        while (input.length() == 0) {
+            System.out.println("ERROR, string must not be empty.");
+
+            System.out.println("Enter a string again: ");
+
+            input = inScanner.nextLine();
+
+            getWordCount(input);
+        }
+            getWordCount(input);
+
+            return input;
+
     }
 
-    /**
-     * Given a String, returns the number of words in the String.  A word is a sequence of 
-     * characters with no spaces.  For example, the method call:
-     * <pre>
-     *      int count = getWordCount("The quick brown fox jumped");
-     * </pre>
-     * results in count having a value of 5. This method should be called from the main method.
-     * For this assignment, you may assume that words will be separated by exactly one space.
-     * @param input The String in question.
-     * @return The number of words in the String. Each word is separated by a single space.
-     */
     private static int getWordCount(String input) {
-        // TODO: Fill in the body
+        
+        int wordCount = input.split(" ").length;
+
+        System.out.println("Your string has " + wordCount + " words in it");
+
+        return wordCount;
     }
 }
